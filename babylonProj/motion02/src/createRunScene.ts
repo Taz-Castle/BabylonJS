80502e1d4eb54b402ebf834ff79fe0c2278f8adc
Gems <b01817384@studentmail.uws.ac.uk>
2025-12-04 15:29:22 +0000

    Collisions
    getting there

--- babylonProj/motion02/src/createRunScene.ts
+++ babylonProj/motion02/src/createRunScene.ts
@@ -1,43 +1,63 @@
+import { AbstractMesh, ActionManager, CubeTexture, Mesh, _ENVTextureLoader } from "@babylonjs/core";
+import { SceneData } from "./interface";
 import {
-  IncrementValueAction,
-  PredicateCondition,
-  SetValueAction,
-} from "@babylonjs/core/Actions";
-import { ActionManager } from "@babylonjs/core/Actions/actionManager";
-import { Scene } from "@babylonjs/core/scene";
-import { Mesh } from "@babylonjs/core";
+  keyActionManager,
+  keyDownMap,
+  keyDownHeld,
+  getKeyDown,
+} from "./keyActionManager";
+import { characterActionManager } from "./characterActionManager";
 
-export function characterActionManager(scene: Scene, character: Mesh) {
-  character.actionManager = new ActionManager(scene);
-  let pickItem = { flag: false };
+import "@babylonjs/core/Materials/Textures/Loaders/envTextureLoader";
+import "@babylonjs/core/Helpers/sceneHelpers";
+export default function createRunScene(runScene: SceneData) {
+  runScene.scene.actionManager = new ActionManager(runScene.scene);
+  keyActionManager(runScene.scene);
 
-  scene.actionManager.registerAction(
-    new IncrementValueAction(
-      ActionManager.OnEveryFrameTrigger,
-      character,
-      "rotation.y",
-      0.1,
-      new PredicateCondition(
-        character.actionManager as ActionManager,
-        function () {
-          return pickItem.flag == true;
-        }
-      )
-    )
+  const environmentTexture = new CubeTexture(
+    "assets/textures/industrialSky.env",
+    runScene.scene
+  );
+  const skybox = runScene.scene.createDefaultSkybox(
+    environmentTexture,
+    true,
+    10000,
+    0.1
   );
 
-  character.actionManager.registerAction(
-    new SetValueAction(ActionManager.OnPickDownTrigger, pickItem, "flag", true)
-  );
+  runScene.scene.onBeforeRenderObservable.add(() => {
+    // check and respond to keypad presses
 
-  character.actionManager.registerAction(
-    new SetValueAction(
-      ActionManager.OnLongPressTrigger,
-      pickItem,
-      "flag",
-      false
-    )
-  );
+
+    runScene.player.then((result) => {
+      let character: AbstractMesh = result!.meshes[0];
+      if (keyDownMap["w"] || keyDownMap["ArrowUp"]) {
+        character.position.x -= 0.1;
+        character.rotation.y = (3 * Math.PI) / 2;
+      }
+      if (keyDownMap["a"] || keyDownMap["ArrowLeft"]) {
+        character.position.z -= 0.1;
+        character.rotation.y = (2 * Math.PI) / 2;
+      }
+      if (keyDownMap["s"] || keyDownMap["ArrowDown"]) {
+        character.position.x += 0.1;
+        character.rotation.y = (1 * Math.PI) / 2;
+      }
+      if (keyDownMap["d"] || keyDownMap["ArrowRight"]) {
+        character.position.z += 0.1;
+        character.rotation.y = (0 * Math.PI) / 2;
+      }
+    });
+  });
+
+// add incremental action to player
+  runScene.player.then((result) => {  
+    let characterMesh = result!.meshes[0];
+    characterActionManager(runScene.scene, characterMesh as Mesh);
+  });
+
+  runScene.scene.onAfterRenderObservable.add(() => {});
+  
 }
 
 
---
+++ babylonProj/vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+export default {
+    // config options
+    server: {
+        fs: {
+          // Allow serving files outside of the root
+          allow: [
+            "../.."
+          ]
+        }
+      },
+    optimizeDeps: { exclude: ["@babylonjs/havok"] }
+}
