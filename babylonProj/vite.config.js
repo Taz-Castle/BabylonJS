5e4fd79c6fd11dad8e002bb52558e968f445e67d
Gems <b01817384@studentmail.uws.ac.uk>
2025-12-13 01:00:51 +0000

    building

--- babylonProj/vite.config.js
+++ babylonProj/vite.config.js
@@ -1,29 +1,17 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-export default defineConfig({
-  // config options
-  server: {
-    fs: {
-      // Allow serving files outside of the root
-      allow: ["../.."],
-    },
-  },
-  optimizeDeps: {
-    exclude: ["@babylonjs/havok"],
-    esbuildOptions: {
-      target: "esnext",
-      supported: {
-        'top-level-await': true //browsers can handle top-level-await features
+// vite.config.js
+export default {
+    // config options
+    server: {
+        fs: {
+          // Allow serving files outside of the root
+          allow: [
+            "../.."
+          ]
         }
-    },
-  },
-  build: {
-    target: "esnext",
-  },
-  esbuild: {
-    target: "esnext",
-    supported: {
-      'top-level-await': true //browsers can handle top-level-await features
-      }
-  },
-});
+      },
+    optimizeDeps: { exclude: ["@babylonjs/havok"] }
+}
+ 
+
+// https://forum.babylonjs.com/t/importing-and-implementing-havok-in-vite-react-ts-project-fails/48441/4
+
